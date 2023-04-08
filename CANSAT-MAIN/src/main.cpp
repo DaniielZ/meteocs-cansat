91b23e1ab33e895e4e188dcb06e6d89361175a21
DaniielZ <34136852+DaniielZ@users.noreply.github.com>
2023-04-08 14:54:47 +0300

    Testing in progress

--- CANSAT-MAIN/src/main.cpp
+++ CANSAT-MAIN/src/main.cpp
@@ -4,8 +4,3 @@ void setup()
 {
     // put your setup code here, to run once:
 }
-
-void loop()
-{
-    // put your main code here, to run repeatedly:
-}
---
+++ CANSAT-NANO/include/states/state.cpp
@@ -0,0 +1,11 @@
+// include much stuff
+
+class State
+{
+public:
+    // objects:
+    // SD CARD
+    // Tx/Rx
+    // Sensors
+    // Sensor data
+};
