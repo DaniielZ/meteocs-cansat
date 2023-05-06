c5ccc6d5df8d0f03b3ce30438fd4dd2f518af376
DaniielZ <34136852+DaniielZ@users.noreply.github.com>
2023-05-06 14:22:30 +0300

    Acc fixed and tested

--- CANSAT-SENSORTEST-NANO/SENSORS_TESTS_NANO/mainH3LIS100.cpp
+++ CANSAT-SENSORTEST-NANO/SENSORS_TESTS_NANO/mainH3LIS100.cpp
@@ -1,73 +1,42 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "H3LIS100.h"
-int sda = 16;
-int scl = 17;
+int sda = 4;
+int scl = 3;
 
-TwoWire acc_wire(i2c0, sda, scl);
+// TwoWire acc_wire(i2c0, sda, scl);
 
-void scan_address()
-{
-    byte error, address;
-    int nDevices;
-
-    Serial.println("Scanning...");
-
-    nDevices = 0;
-    for (address = 1; address < 127; address++)
-    {
-        // The i2c_scanner uses the return value of
-        // the Write.endTransmisstion to see if
-        // a device did acknowledge to the address.
-        acc_wire.beginTransmission(address);
-        error = acc_wire.endTransmission();
-
-        if (error == 0)
-        {
-            Serial.print("I2C device found at address 0x");
-            if (address < 16)
-                Serial.print("0");
-            Serial.print(address, HEX);
-            Serial.println("  !");
-
-            nDevices++;
-        }
-        else if (error == 4)
-        {
-            Serial.print("Unknown error at address 0x");
-            if (address < 16)
-                Serial.print("0");
-            Serial.println(address, HEX);
-        }
-    }
-    if (nDevices == 0)
-        Serial.println("No I2C devices found\n");
-    else
-        Serial.println("done\n");
-}
 void setup()
 {
-    Serial.begin();
+    Serial.begin(9600);
     while (!Serial)
     {
         delay(100);
     }
     delay(500);
-    scan_address();
     H3LIS100 acc(123);
-    if (acc.begin(24u, &acc_wire))
+    Wire2.setSCL(scl);
+    Wire2.setSDA(sda);
+    if (!acc.begin(0x19, &Wire2))
     {
-        Serial.println("alles good");
+        Serial.println("alles bad");
+        while (true)
+        {
+            delay(100);
+        }
     }
     Serial.println("alles good");
+    delay(1000);
     while (true)
     {
         sensors_event_t event;
         acc.getEvent(&event);
-        Serial.print(event.acceleration.x);
-        Serial.print(event.acceleration.y);
-        Serial.println(event.acceleration.z);
-        delay(500);
+        Serial.print(event.acceleration.x, 4);
+        Serial.print(" ");
+        Serial.print(event.acceleration.y, 4);
+        Serial.print(" ");
+        Serial.println(event.acceleration.z, 4);
+        delay(100);
     }
 }
 void loop()
